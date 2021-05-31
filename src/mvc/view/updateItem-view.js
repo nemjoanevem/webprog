0053780727a1fdf585286b471ffc615b4c6cf887
krepepe <peti981012@gmail.com>
2021-05-31 10:54:19 +0200

    update item view

---
+++ src/mvc/view/updateItem-view.js
@@ -0,0 +1,40 @@
+class UpdateItem
+{
+    constructor(id, parent)
+    {
+        this.id = id;
+        this._elem = document.createElement("form");
+        this._list = ItemModel.GetList();
+        for(var i=0; i<this._list.length; i++){
+            if(this._list[i].id == this.id){
+                this.item = this._list[i];
+            }
+        }
+
+        this._elem.innerHTML = `
+        <label>Új név:</label><br>
+        <input type="text" id="newName" value="${this.item.name}"><br>
+        <label>Új ár:</label><br>
+        <input type="number" id="newPrice" value="${this.item.price}">
+        <button class="save">Mentés</button>
+        <button class="cancel">Mégsem</button>`
+
+
+
+        parent.appendChild(this._elem);
+
+    }
+
+    onSaveClick(method)
+    {
+        this._elem.querySelector('.save').onclick = (e) =>
+        {
+            this.newName = document.getElementById("newName").value;
+            this.newPrice = document.getElementById("newPrice").value;
+            e.preventDefault();
+            method(this);
+        }
+    }
+
+
+}
