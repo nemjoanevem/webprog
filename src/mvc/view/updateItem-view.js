5ee1276e750e8688b1924a34dc875df6a59e8637
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-06-17 13:31:20 +0200

    adding fade-down

--- src/mvc/view/updateItem-view.js
+++ src/mvc/view/updateItem-view.js
@@ -3,7 +3,7 @@ class UpdateItem
     constructor(id, parent)
     {
         this.id = id;
-        this._elem = document.createElement("form");
+        this._elem = document.createElement("section");
         this._list = ItemModel.GetList();
         for(var i=0; i<this._list.length; i++){
             if(this._list[i].id == this.id){
@@ -12,12 +12,14 @@ class UpdateItem
         }
 
         this._elem.innerHTML = `
+        <form>
         <label class="newNameLbl">Új név:</label><br>
         <input type="text" id="newName" value="${this.item.name}"><br>
         <label class="newPriceLbl">Új ár:</label><br>
         <input type="number" id="newPrice" value="${this.item.price}">
         <button class="save">Mentés</button>
-        <button class="cancel">Mégsem</button>`
+        <button class="cancel">Mégsem</button>
+        </form>`
 
 
 
